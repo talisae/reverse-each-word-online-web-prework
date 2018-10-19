d50dac4be9bf471fe07f519bc24e17e21b5c49fe
Talisa Erazo <talisaerazo@gmail.com>
2018-10-19 03:35:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   array = string.split(" ")
   newArray = []
-  array.each do
-    string.reverse
+  array.each do |string|
+    newArray << string.reverse
   end
 end
