327500b0fdb1bb52cfc9d6687fcbbdbab566def2
Talisa Erazo <talisaerazo@gmail.com>
2018-10-19 03:39:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,21 @@ def reverse_each_word (string)
   end
   newArray.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  newArray = []
+  array.collect do |string|
+    newArray << string.reversed
+  end
+  newArray.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
